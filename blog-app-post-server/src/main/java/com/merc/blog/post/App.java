5fc08a60e4dfaf9bb92b74630e079347b290bd05
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-30 09:49:09 +0530

    eureka

--- blog-app-post-server/src/main/java/com/merc/blog/post/App.java
+++ blog-app-post-server/src/main/java/com/merc/blog/post/App.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BlogAppPostServerApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogAppPostServerApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
